9422ea894c9073614b0bb0f65054bb13d6ae469c
Rohit Vemuri <rohitvemuri@yahoo.com>
2022-04-05 21:31:34 -0700

    Added starter project

---
+++ ClassPass/ClassPass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassPass
+//
+//  Created by Rohit Vemuri on 4/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
