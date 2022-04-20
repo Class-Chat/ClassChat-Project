eb30457f73efca5ac062853ea966770f58764a3d
“akheel-s” <akheelmshaik@gmail.com>
2022-04-19 19:01:35 -0700

    Setup initial firebase files to connect with the firebase database.

---
+++ ClassPass/Pods/Target Support Files/Pods-ClassPass-ClassPassUITests/Pods-ClassPass-ClassPassUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ClassPass_ClassPassUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ClassPass_ClassPassUITests
+@end
---
+++ ClassPass/Pods/Target Support Files/Pods-ClassPass/Pods-ClassPass-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ClassPass : NSObject
+@end
+@implementation PodsDummy_Pods_ClassPass
+@end
